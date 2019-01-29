e95d8aad4fdc648a18d9b4f83e15fa895223f077
Ryan Mui <reachryanmui@gmail.com>
2019-01-29 02:23:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
-def oxford_comma(array)
+singlearray = ["starpower"]
+doublearray = ["chocolate", "vanilla"]
+array = ["hippo", "giraffe", "monkey", "horse"]
 
+def oxford_comma(array)
+returnstring = ""
+  while array.length > 2
+    returnstring << array.shift + ", "
+  end
+  if array.length == 2  && !returnstring.empty?
+    returnstring << array.shift + ", and " + array.shift
+  elsif returnstring.empty?
+    returnstring << array.join(" and ")
+  elsif array.length == 1
+    returnstring = array.join
+  end
+return returnstring
 end
+
+puts oxford_comma(singlearray)
+puts oxford_comma(doublearray)
+puts oxford_comma(array)
