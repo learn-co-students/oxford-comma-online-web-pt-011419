4b00c41ef9c55e40e89e5eed628479a3e6168259
David Acosta <davidacosta25081@yahoo.com>
2019-01-30 05:53:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+  if array.length == 1
+      return "#{array[0]}"
+    elsif array.length == 2
+      return array.join(" and ")
 
+    elsif array.length >= 3
+
+last_item = ", and #{array.pop}"
+string_variable = array.join(", ")
+string_variable <<  last_item
+end
 end
